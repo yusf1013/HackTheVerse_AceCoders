b85819a3a2ad46c8b872a25ecc1a1882229833e7
bsse1017kavi <bsse1017@iit.du.ac.bd>
2020-11-14 18:32:02 +0600

    Last added location traced

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
         // home: LoginScreen(),
         //home: AdminHomePage(),
         //home: Scanner(),
-        home: LocationTracking(),
+        //home: LocationTracking(),
         debugShowCheckedModeBanner: false,
       ),
     );
