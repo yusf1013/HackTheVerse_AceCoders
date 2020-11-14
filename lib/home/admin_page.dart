5938295feacafb704c4d3dcbce788e0886b7f289
bsse1017kavi <bsse1017@iit.du.ac.bd>
2020-11-15 02:00:41 +0600

    Started Admin UI

---
+++ lib/home/admin_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AdminPage extends StatefulWidget {
+  @override
+  _AdminPageState createState() => _AdminPageState();
+}
+
+class _AdminPageState extends State<AdminPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        //backgroundColor: Colors.red,
+        title: Text(
+          "Admin Panel",
+        ),
+      ),
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
